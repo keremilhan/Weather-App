c7568a9bb8a7c7266c93472007e8448a95d7a4b1
Kerem ILHAN <kerem7095@gmail.com>
2022-05-19 08:26:55 +0300

    Styling and JS Updated

---
+++ script.js
@@ -0,0 +1,15 @@
+let weather = {
+    apiKey: "8fd372a2a8e20a86c2e50cbc97e57005",
+    fetchWeather: function(city){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+    }
+};
